1b147be739e216b3ecb16b654091d38f3a121d45
Erik Azzarano <eja4296@rit.edu>
2018-10-28 11:33:08 -0400

    Copied files. Started making files for new game (called 'Azzarano').

---
+++ Assets/Scripts/Games/Azzarano/AzzaranoTrial.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+
+
+/// <summary>
+/// Contains Trial data for the Azzarano gameType.
+/// </summary>
+public class AzzaranoTrial : Trial
+{
+	/// <summary>
+	/// The duration the stimulus will be shown for.
+	/// </summary>
+	public float duration = 0;
+
+
+	#region ACCESSORS
+
+	public float Duration
+	{
+		get
+		{
+			return duration;
+		}
+	}
+
+	#endregion
+
+
+	public AzzaranoTrial(SessionData data, XmlElement n = null) 
+		: base(data, n)
+	{
+	}
+	
+
+	/// <summary>
+	/// Parses Game specific variables for this Trial from the given XmlElement.
+	/// If no parsable attributes are found, or fail, then it will generate some from the given GameData.
+	/// Used when parsing a Trial that IS defined in the Session file.
+	/// </summary>
+	public override void ParseGameSpecificVars(XmlNode n, SessionData session)
+	{
+		base.ParseGameSpecificVars(n, session);
+
+		AzzaranoData data = (AzzaranoData)(session.gameData);
+		if (!XMLUtil.ParseAttribute(n, AzzaranoData.ATTRIBUTE_DURATION, ref duration, true))
+		{
+			duration = data.GeneratedDuration;
+		}
+	}
+
+
+	/// <summary>
+	/// Writes any tracked variables to the given XElement.
+	/// </summary>
+	public override void WriteOutputData(ref XElement elem)
+	{
+		base.WriteOutputData(ref elem);
+		XMLUtil.CreateAttribute(AzzaranoData.ATTRIBUTE_DURATION, duration.ToString(), ref elem);
+	}
+}
